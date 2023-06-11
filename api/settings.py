acb290f0ed7300cc342a77a34b4637cd73303adb
Joshua <akinleyejoshua.dev@gmail.com>
2023-06-11 03:47:20 +0100

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGIN = True
 CORS_ALLOWED_ORIGINS = [
     "https://facialanalysis.netlify.app"
 ]
